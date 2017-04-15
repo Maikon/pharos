99da743205a419bcaeea1c8fa125e2becb8014c8
Georgina McFadyen <gemcfadyen@hotmail.co.uk>
2017-04-15 12:30:23 +0100

    Updating the documentation for the PostService

--- apps/post_service/lib/post_service.ex
+++ apps/post_service/lib/post_service.ex
@@ -3,9 +3,9 @@ defmodule PostService do
   Stores a posts
 
   ## Examples
-      iex> PostService.create("www.mypost.com", "a great post on me")
-      iex> %DbService.Post{__meta__: #Ecto.Schema.Metadata<:loaded, "posts">,
-           description: "a great post on me", id: 14, name: "www.post.com"}
+    iex> PostService.create("www.mypost.com", "a great post on me")
+    %DbService.Post{__meta__: #Ecto.Schema.Metadata<:loaded, "posts">, description: "a great post on me", id: 14, name: "www.post.com"}
+
   """
   @spec create(String.t, String.t) :: struct
   def create(new_post, description) do
@@ -17,11 +17,11 @@ defmodule PostService do
   Retrieves all the posts
 
   ## Examples
-      iex> PostService.create("www.mypost.com", "a great post on me")
-      iex> PostService.create("www.anotherpost.com", "another great post on me")
-      iex> PostService.all()
-      iex> [%DbService.Post{__meta__: #Ecto.Schema.Metadata<:loaded, "posts">, description: "a great post on me", id: 13, name: "www.mypost.com"},
-            %DbService.Post{__meta__: #Ecto.Schema.Metadata<:loaded, "posts">, description: "another great post on me", id: 13, name: "www.anotherpost.com"}]
+    iex> PostService.create("www.mypost.com", "a great post on me")
+    iex> PostService.create("www.anotherpost.com", "another great post on me")
+    iex> PostService.all()
+    [%DbService.Post{__meta__: #Ecto.Schema.Metadata<:loaded, "posts">, description: "a great post on me", id: 13, name: "www.mypost.com"}, %DbService.Post{__meta__: #Ecto.Schema.Metadata<:loaded, "posts">, description: "another great post on me", id: 13, name: "www.anotherpost.com"}]
+
   """
   @spec all() :: list(struct)
   def all() do
