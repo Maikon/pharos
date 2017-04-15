f2d362e70216f2fc8bde286e698fe323b2143c56
Georgina McFadyen <gemcfadyen@hotmail.co.uk>
2017-04-15 12:13:59 +0100

    Updating documentation

--- apps/db_service/lib/db_service.ex
+++ apps/db_service/lib/db_service.ex
@@ -1,23 +1,26 @@
 defmodule DbService do
+
   @doc """
-  Persist a post to the database
+  Persist a post entity to the database
 
   ## Examples
-       iex> DbService.persist("post", %{post_url:  "a-url", description: "a-description"})
-       iex> %DbService.Post{__meta__: #Ecto.Schema.Metadata<:loaded, "posts">,    description: "a-description", id: 15, name: "a-url"}
+      iex> DbService.persist(%{post_url: "a-url", description: "a-description"})
+      %DbService.Post{__meta__: #Ecto.Schema.Metadata<:loaded, "posts">, description: "a-description", id: 24, name: "a-url"}
+
   """
-  @spec persist(String.t, map) :: %DbService.Post{}
-  def persist(service_name, data) do
+  @spec persist(map) :: %DbService.Post{}
+  def persist(data) do
     DbService.PostRepository.create(data[:post_url], data[:description])
   end
 
   @doc """
-  Retrieves all posts from the database
+  Retrieves all post entities from the database
 
   ## Examples
-      iex> DbService.persist("post", %{post_url:  "a-url", description: "a-description"})
+      iex> DbService.persist(%{post_url: "a-url", description: "a-description"})
       iex> DbService.all()
-      iex> [%DbService.Post{__meta__: #Ecto.Schema.Metadata<:loaded, "posts"> description: "a-description", id: 15, name: "a-url"}]
+      [%DbService.Post{__meta__: #Ecto.Schema.Metadata<:loaded, "posts">, description: "a-description", id: 24, name: "a-url"}]
+
   """
   @spec all() :: list(%DbService.Post{})
   def all() do
--- apps/db_service/test/db_service_test.exs
+++ apps/db_service/test/db_service_test.exs
@@ -8,15 +8,15 @@ defmodule DbServiceTest do
   test "post is persisted" do
     post_to_save =  %{post_url: "post-url", description: "a-description"}
 
-    post_persisted = DbService.persist("post", post_to_save)
+    post_persisted = DbService.persist(post_to_save)
 
     assert post_persisted.name == post_to_save[:post_url]
     assert post_persisted.description == post_to_save[:description]
   end
 
   test "retrieves all posts" do
-    DbService.persist("post", %{post_url: "post-url", description: "a-description"})
-    DbService.persist("post", %{post_url: "another-post-url", description: "another-description"})
+    DbService.persist(%{post_url: "post-url", description: "a-description"})
+    DbService.persist(%{post_url: "another-post-url", description: "another-description"})
 
     results = DbService.all()
 
