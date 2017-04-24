b7c7da17d742304f7b59dce041105b641dc7deb5
Georgina McFadyen <gemcfadyen@hotmail.co.uk>
2017-04-24 19:40:30 +0100

    Replacing incorrect quotes

--- apps/db_service/config/prod.exs
+++ apps/db_service/config/prod.exs
@@ -5,8 +5,8 @@ config :logger, level: :info
 config :db_service, DbService.Repo,
   adapter: Ecto.Adapters.Postgres,
   ssl: true,
-  username: “${DB_USER}“,
-  password: “${DB_PASSWORD}“,
-  database: “${DB_NAME}“,
-  hostname: “${DB_HOST}“,
+  username: "${DB_USER}",
+  password: "${DB_PASSWORD}",
+  database: "${DB_NAME}",
+  hostname: "${DB_HOST}",
   pool_size: 20
