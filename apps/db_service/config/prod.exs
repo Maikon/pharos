a38fa109e45a43a796fc1dc8de7b58b5ebc115e8
Makis Otman <makisotman@gmail.com>
2017-05-01 17:08:53 +0100

    Fix db_service prod config and remove prod.secret

--- apps/db_service/config/prod.exs
+++ apps/db_service/config/prod.exs
@@ -5,8 +5,8 @@ config :logger, level: :info
 config :db_service, DbService.Repo,
   adapter: Ecto.Adapters.Postgres,
   ssl: false,
-  username: "System.get_env(DB_USER)",
-  password: "System.get_env(DB_PASSWORD)",
-  database: "System.get_env(DB_NAME)",
-  hostname: "System.get_env(DB_HOST)",
+  username: System.get_env("DB_USER"),
+  password: System.get_env("DB_PASSWORD"),
+  database: System.get_env("DB_NAME"),
+  hostname: System.get_env("DB_HOST"),
   pool_size: 20
