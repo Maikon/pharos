8706f132dbc6709eb744a672c65206256343b740
Georgina McFadyen <gemcfadyen@hotmail.co.uk>
2017-05-02 20:02:48 +0100

    Removing memorydb references

--- apps/pharos_web/web/controllers/search_controller.ex
+++ apps/pharos_web/web/controllers/search_controller.ex
@@ -2,7 +2,10 @@ defmodule PharosWeb.SearchController do
   use PharosWeb.Web, :controller
 
   def index(conn, _params) do
-    results = Twitter.MemoryDb.all()
+    results =
+      [
+        {"A-Topic", [%{title: "a-title", description: "a-description", link: "a-link", source: :twitter}]}
+      ]
 
     render conn, "index.html", results: results
   end
@@ -10,7 +13,6 @@ defmodule PharosWeb.SearchController do
   def search(conn, query) do
     results = Twitter.Search.execute(query)
 
-    Twitter.MemoryDb.store(query["topic"], results)
     redirect conn, to: search_path(conn, :index)
   end
 end
