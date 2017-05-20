8a0440a8a7510e3cb451094c086b5e4ee9a9a103
Georgina McFadyen <gemcfadyen@hotmail.co.uk>
2017-05-20 09:24:23 +0100

    Wiki search implemented

---
+++ apps/wikipedia/lib/response.ex
@@ -0,0 +1,24 @@
+defmodule Response do
+
+  def convert({:ok, %HTTPoison.Response{body: body, status_code: _status_code}}) do
+    body
+    |> JSON.decode!
+    |> List.delete_at(0)
+    |>format
+  end
+
+  def convert({:error, %HTTPoison.Error{id: _id, reason: reason} }) do
+    IO.inspect "Error in http call #{reason}"
+  end
+
+  defp format(response_details) do
+    response_details
+    |> Enum.zip
+    |> Enum.map(fn(x) -> create_struct(x) end)
+  end
+
+  defp create_struct({title, description, link}) do
+    %Result{title: title, description: description, link: link, source: 'wikipedia'}
+  end
+end
+
